43aa4acd5dbbc8e9f9261c42ba874ca82e75b010
KutlayTunc <kutlay-tunc@hotmail.com>
2022-07-28 16:44:25 +0300

    Add Context. Login, register, home components are revised

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import { UserAuth } from "../../Context/AuthContext"
 
 const Home = () => {
+  const navigate = useNavigate()
+  const { user, logout } = UserAuth()
+  const handleLogoutButtonPress = async () => {
+    try {
+      await logout()
+      navigate("/login")
+      console.log("Logged out")
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <div>User Email:{user && user.email}</div>
+      <button onClick={handleLogoutButtonPress}>Logout</button>
+    </div>
   )
 }
 
--- src/Components/MainRouter/MainRouter.js
+++ src/Components/MainRouter/MainRouter.js
@@ -1,17 +1,18 @@
-import { Routes, Route, Link } from "react-router-dom"
-
+import { Routes, Route } from "react-router-dom"
+import { AuthContextProvider } from "../../Context/AuthContext"
 import Home from "../Home/Home"
 import Login from "../Login/Login"
 import Register from "../Register/Register"
 
 const MainRouter = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </AuthContextProvider>
   )
 }
 
