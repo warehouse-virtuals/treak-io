d1b63385615cae2c39b03f0c8329967590fea135
KutlayTunc <kutlay-tunc@hotmail.com>
2022-09-06 00:29:22 +0300

    Add new repair request form, navbar logo changed

--- src/UITools/Button.js
+++ src/UITools/Button.js
@@ -4,7 +4,7 @@ const Button = ({ label, icon, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="px-10 py-3 bg-[#605bff] hover:bg-[#7a75ff] text-white font-bold rounded-lg mt-10 text-lg select-none"
+      className="px-10 py-3 bg-gradient-to-r from-[#605bff] to-[#6865d1] hover:bg-[#7a75ff] text-white font-bold rounded-lg mt-10 text-lg select-none"
     >
       {label}
     </div>
--- src/UITools/TextInput.js
+++ src/UITools/TextInput.js
@@ -1,19 +1,14 @@
 import React from "react"
 
-const TextInput = ({ type, label, inputRef, placeholder, onInput }) => {
+const TextInput = ({ type, label, inputRef, placeholder, onInput, addCSS }) => {
   return (
-    <div className="w-2/3  flex justify-center items-start flex-col">
-      <div
-        className="text-xl font-medium mb-2 mt-5 text-slate-700 dark:text-white"
-      >{label}</div>
-      <input
-        onInput={onInput}
-        ref={inputRef}
-        className="w-full border-2 border-slate-300 h-14 rounded-lg px-5 text-slate-700"
-        type={type}
-        placeholder={placeholder}
-      />
-    </div>
+    <input
+      onInput={onInput}
+      ref={inputRef}
+      className={`${addCSS} flex justify-center items-start flex-col border-slate-200 h-14 px-5 text-slate-700`}
+      type={type}
+      placeholder={placeholder}
+    />
   )
 }
 
