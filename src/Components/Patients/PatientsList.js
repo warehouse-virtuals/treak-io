491c1071700439329f29778f1deeca57df266e17
KutlayTunc <kutlay-tunc@hotmail.com>
2022-11-13 22:33:29 +0300

    Fixing css of Patients.js

--- src/Components/Patients/PatientsList.js
+++ src/Components/Patients/PatientsList.js
@@ -1,7 +1,11 @@
+import "./PatientsList.css"
+import { FiPlus } from "react-icons/fi"
 import { toDate } from "date-fns"
 import { useTranslation } from "react-i18next"
 import { useState, useEffect } from "react"
 import { UserAuth } from "../../Context/AuthContext"
+
+import { useNavigate } from "react-router-dom"
 import PatientOverview from "./PatientOverview"
 
 const PatientsList = () => {
@@ -9,6 +13,7 @@ const PatientsList = () => {
   const [focusedPatient, setFocusedPatient] = useState({})
   const { getPatients, userData } = UserAuth()
   const { t } = useTranslation("patients")
+  const navigate = useNavigate()
 
   const fetchPatientData = async () => {
     return await getPatients(userData.customerID, userData.clinicID)
@@ -43,10 +48,18 @@ const PatientsList = () => {
     `${t("DOB")}`,
     `${t("GENDER")}`,
   ]
+  const handleAddPatientButtonClick = async () => {
+    try {
+      navigate("/addPatient")
+      console.log("Clicked Add Button")
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   const TableHeadItem = ({ item }) => {
     return (
-      <div className='pl-3 gap-10 grid grid-cols-5 mb-3 '>
+      <div className='patients-table-head-item '>
         {item.map((h, index) => {
           return (
             <div key={index} className=''>
@@ -60,12 +73,12 @@ const PatientsList = () => {
   const TableRow = ({ data, patient }) => {
     return (
       <div
-        className='grid border-r-8 border-green-400  hover:bg-slate-100 transition-all pl-5 gap-10 items-center grid-cols-5 text-sm mb-1 h-14 rounded-2xl drop-shadow-sm bg-white'
+        className='patients-table-row-item'
         onClick={() => setFocusedPatient(patient)}
       >
         {data.map((item, index) => {
           return (
-            <div className='border-r-2 border-slate-100 ' key={index}>
+            <div className='patients-table-row-item-content' key={index}>
               {item}
             </div>
           )
@@ -75,8 +88,8 @@ const PatientsList = () => {
   }
   const Table = ({ theadData, tbodyData, customClass }) => {
     return (
-      <div className={customClass}>
-        <div className='text-[#c4c8d5] text-sm font-semibold'>
+      <div className='patients-table-container'>
+        <div className='patients-table-head-container'>
           <TableHeadItem item={theadData} />
         </div>
         <div className=''>
@@ -96,15 +109,21 @@ const PatientsList = () => {
 
   console.log(focusedPatient)
   return (
-    <div className='flex  justify-around w-full'>
-      <div className='flex w-2/3'>
+    <div className='patient-list-container'>
+      <div className='patient-list-header-container'>
+        <div
+          onClick={handleAddPatientButtonClick}
+          className='patients-add-patient-btn'
+        >
+          <FiPlus size={30} stroke='#35d8ac' className='' /> Kullanıcı Ekle
+        </div>
         <Table
           theadData={theadData}
           tbodyData={tbodyData}
           customClass='w-full font-normal '
         />
       </div>
-      <div className='flex w-[400px]'>
+      <div className='patients-overview-container'>
         <PatientOverview
           focusedPatientData={focusedPatient}
           patientDeleted={(confirm) => {
